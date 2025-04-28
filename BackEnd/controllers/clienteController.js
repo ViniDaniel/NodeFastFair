5b319d7ffb040aba052c2360a880cb1949692a64
ViniDaniel <viniciusdanielgonsalvess@gmail.com>
2025-04-28 20:37:30 -0300

    Atualização completa do CRUD de cliente, com busca completa, busca unica, delete e update

--- BackEnd/controllers/clienteController.js
+++ BackEnd/controllers/clienteController.js
@@ -14,13 +14,84 @@ const clienteController = {
 
       const response = await ClienteModel.create(cliente);
 
-      return res.status(201).json({ response, message: "Cliente criado com sucesso." });
-      
+      return res
+        .status(201)
+        .json({ response, message: "Cliente criado com sucesso." });
     } catch (error) {
       if (error.code === 11000) {
-        return res.status(400).json({ message: "CPF, email ou celular já cadastrado." });
+        return res
+          .status(400)
+          .json({ message: "CPF, email ou celular já cadastrado." });
       }
-      return res.status(500).json({ message: "Erro ao criar cliente." });
+      return res
+        .status(500)
+        .json({ message: "Erro ao criar cliente.", error: error.message });
+    }
+  },
+  getAll: async (req, res) => {
+    try {
+      const cliente = await ClienteModel.find();
+      return res.status(200).json(cliente);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Erro interno" }); //500 é um erro interno
+    }
+  },
+  get: async (req, res) => {
+    try {
+      const id = req.params.id; //pega o id do usuario
+      const cliente = await ClienteModel.findById(id);
+
+      if (!cliente) {
+        return res.status(404).json({ message: "Cliente não encontrado!" }); //404 é Not Found
+      }
+      return res.status(200).json(cliente);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Erro interno" });
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const cliente = await ClienteModel.findById(id);
+      if (!cliente) {
+        return res.status(404).json({ message: "Cliente não encontrado!" });
+      }
+      const deleteCliente = await ClienteModel.findByIdAndDelete(id);
+      return res
+        .status(200)
+        .json({ deleteCliente, message: "Cliente deletado com suceso" });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json({ message: "Error interno", error: error.message });
+    }
+  },
+  update: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const { nome, cpf, email, celular, genero } = req.body;
+
+      const cliente = { nome, cpf, email, celular, genero };
+
+      const updateCliente = await ClienteModel.findByIdAndUpdate(id, cliente, {
+        new: true,
+      });
+
+      if (!updateCliente) {
+        return res.status(404).json({ message: "Cliente não encontrado" });
+      }
+
+      return res
+        .status(200)
+        .json({ updateCliente, message: "Cliente atualizado com sucesso!" });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json({ message: "Erro interno!", error: error.message });
     }
   },
 };
--- BackEnd/routes/clienteRouter.js
+++ BackEnd/routes/clienteRouter.js
@@ -1,7 +1,27 @@
 const router = require("express").Router();
 const clienteController = require("../controllers/clienteController");
 
-router.route("/clientes").post((req, res) => clienteController.create(req, res));
+//rota de criação de um cliente
+router
+  .route("/clientes")
+  .post((req, res) => clienteController.create(req, res));
 
+  //rota de busca de todos os clientes
+router.route("/clientes").get((req, res) => clienteController.getAll(req, res));
+
+//rota de busca de um cliente específico
+router
+  .route("/clientes/:id")
+  .get((req, res) => clienteController.get(req, res));
+
+  //rota de delete do cliente
+router
+  .route("/clientes/:id")
+  .delete((req, res) => clienteController.delete(req, res));
+
+  //rota de atualização do cliente
+router
+  .route("/clientes/:id")
+  .put((req, res) => clienteController.update(req, res));
 
 module.exports = router;
