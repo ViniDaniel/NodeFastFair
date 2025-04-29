d6084b9076e327998e89376e0f3fbec3bdadf7c9
ViniDaniel <viniciusdanielgonsalvess@gmail.com>
2025-04-28 21:15:31 -0300

    Atualização completa do CRUD de feirante, com busca completa, busca unica, delete e update

--- BackEnd/controllers/feiranteController.js
+++ BackEnd/controllers/feiranteController.js
@@ -39,14 +39,110 @@ const feiranteController = {
 
       const response = await FeiranteModel.create(feirante);
 
-      return res.status(201).json({ response, message: "feirante criado com sucesso." });
-
+      return res
+        .status(201)
+        .json({ response, message: "feirante criado com sucesso." });
     } catch (error) {
       if (error.code === 11000) {
-        return res.status(400).json({ message: "CPF/CNPJ, email ou celular já cadastrado." });
+        return res
+          .status(400)
+          .json({ message: "CPF/CNPJ, email ou celular já cadastrado." });
       }
-      return res.status(500).json({ message: "Erro ao criar feirante.", error: error.message });
+      return res
+        .status(500)
+        .json({ message: "Erro ao criar feirante.", error: error.message });
+    }
+  },
+  getAll: async (req, res) => {
+    try {
+      const feirante = await FeiranteModel.find();
+      return res.status(200).json(feirante);
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json({ message: "Erro interno", error: error.message });
+    }
+  },
+  get: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const feirante = await FeiranteModel.findById(id);
+      if (!feirante) {
+        return res.status(404).json({ message: "Feirante não encontrado!" });
+      }
+      return res.status(200).json(feirante);
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json({ message: "Erro interno", error: error.message });
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const feirante = await FeiranteModel.findById(id);
+      if (!feirante) {
+        return res.status(404).json({ message: "Feirante não encontrado" });
+      }
+      const deleteFeirante = await FeiranteModel.findByIdAndDelete(id);
+      return res
+        .status(200)
+        .json({ deleteFeirante, message: "Feirante deletado com sucesso!" });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json({ message: "Erro interno", error: error.message });
+    }
+  },
+  update: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const {
+        nome,
+        cpf_cnpj,
+        email,
+        celular,
+        genero,
+        cep,
+        endereco,
+        numeroCasa,
+        bairro,
+        cidade,
+        uf,
+      } = req.body;
+      const feirante = {
+        nome,
+        cpf_cnpj,
+        email,
+        celular,
+        genero,
+        cep,
+        endereco,
+        numeroCasa,
+        bairro,
+        cidade,
+        uf,
+      };
+      const updateFeirante = await FeiranteModel.findByIdAndUpdate(
+        id,
+        feirante,
+        { new: true }
+      );
 
+      if (!updateFeirante) {
+        return res.status(404).json({ message: "Feirante não encontrado! " });
+      }
+      return res
+        .status(200)
+        .json({ updateFeirante, message: "Feirante atualizado com sucesso! " });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json({ message: "Erro interno!", error: error.message });
     }
   },
 };
--- BackEnd/routes/feiranteRouter.js
+++ BackEnd/routes/feiranteRouter.js
@@ -1,6 +1,29 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const feiranteController = require("../controllers/feiranteController");
 
-router.route("/feirantes").post((req, res) => feiranteController.create(req, res));
+//criação do feirante
+router
+  .route("/feirantes")
+  .post((req, res) => feiranteController.create(req, res));
+
+//Busca completa
+router
+  .route("/feirantes")
+  .get((req, res) => feiranteController.getAll(req, res));
+
+//Busca individual
+router
+  .route("/feirantes/:id")
+  .get((req, res) => feiranteController.get(req, res));
+
+//Delete do feirante
+router
+  .route("/feirantes/:id")
+  .delete((req, res) => feiranteController.delete(req, res));
+
+//Atualização do feirante
+router
+  .route("/feirantes/:id")
+  .put((req, res) => feiranteController.update(req, res));
 
 module.exports = router;
