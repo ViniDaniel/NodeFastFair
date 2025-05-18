6e32a11f8f0c54ec339c4ced499fa0da308630aa
ViniDaniel <viniciusdanielgonsalvess@gmail.com>
2025-05-18 20:53:15 -0300

    Modificação no navbar para aparecer um menu referente ao feirante, diferente do menu geral. SideBar para centralizar melhor o login e o cadastro

--- FrontEnd/src/components/layout/Navbar.jsx
+++ FrontEnd/src/components/layout/Navbar.jsx
@@ -1,35 +1,47 @@
 import { Link } from "react-router-dom";
-import Container from "./Container";
-import styles from '../../styles/layout_styles/NavBar.module.css';
+import styles from "../../styles/layout_styles/NavBar.module.css";
 import logo from "../../assets/logo.png";
-import Header from "./Header";
+import SidebarMenu from "./SidebarMenu";
+import { useContext, useState } from "react";
+import { FeiranteContext } from "../context/FeiranteContext";
+import { FaBars } from "react-icons/fa";
+
 function NavBar() {
+  const { feirante } = useContext(FeiranteContext);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <nav className={styles.navbar}>
-            <Container>
-            <ul className={styles.list}>
-                <li className={styles.img}>
-                    <img src={logo} alt="Fast&Fair" />
-                </li>
-                <li className={styles.item}>
-                    <Link to="/">Início</Link>
-                </li>
-                <li className={styles.item}>
-                    <Link to="/loginCliente">Login Cliente</Link>
-                </li>
-                <li className={styles.item}>
-                    <Link to="/loginFeirante">Login Feirante</Link>
-                </li>
-                <li className={styles.item}>
-                    <Link to="/suporte">Suporte</Link>
-                </li>
-            </ul>
-            
-            </Container>  
-            <Header />
-        </nav>
+  <div className={styles.left}>
+    <img src={logo} alt="Fast&Fair" className={styles.logo} />
+    <ul className={styles.list}>
+      {!feirante ? (
+        <>
+          <li><Link to="/">Início</Link></li>
+          <li><Link to="/produtos">Produtos</Link></li>
+          <li><Link to="/sobre">Sobre</Link></li>
+          <li><Link to="/suporte">Suporte</Link></li>
+        </>
+      ) : (
+        <>
+          <li><Link to="/descricao">Descrição</Link></li>
+          <li><Link to="/adicionar-produto">Adicionar Produto</Link></li>
+          <li><Link to="/estoque">Estoque</Link></li>
+          <li><Link to="/perfilFeirante">Perfil</Link></li>
+        </>
+      )}
+    </ul>
+  </div>
+
+  <div className={styles.menuIcon} onClick={() => setSidebarOpen(!sidebarOpen)}>
+    <FaBars />
+  </div>
+
+  <SidebarMenu isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} feirante={feirante} />
+</nav>
+
+
   );
 }
 
 export default NavBar;
-
---
+++ FrontEnd/src/components/layout/SidebarMenu.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import styles from "../../styles/layout_styles/SidebarMenu.module.css";
+
+function SidebarMenu({ isOpen, onClose }) {
+  return (
+    <div className={`${styles.sidebar} ${isOpen ? styles.open : ""}`}>
+      <button className={styles.closeBtn} onClick={onClose}>X</button>
+      <h3>Cliente</h3>
+      <ul>
+        <li><Link to="/cadastroCliente" onClick={onClose}>Cadastro</Link></li>
+        <li><Link to="/loginCliente" onClick={onClose}>Login</Link></li>
+        {/*<li><Link to="/carrinho" onClick={onClose}>Carrinho</Link></li>*/}
+      </ul>
+
+      <h3>Feirante</h3>
+      <ul>
+        <li><Link to="/cadastroFeirante" onClick={onClose}>Cadastro</Link></li>
+        <li><Link to="/loginFeirante" onClick={onClose}>Login</Link></li>
+      </ul>
+    </div>
+  );
+}
+
+export default SidebarMenu;
