5f8be8c1aff31817b0afb024016f09a8a35100d8
ViniDaniel <viniciusdanielgonsalvess@gmail.com>
2025-06-01 23:44:40 -0300

    Adição de dados da pagina de carrinho, css para a pagina, modificação no card de produto para carregar produtos pro carrinho para quem estiver logado no site como cliente

--- FrontEnd/src/components/pages/client/shop/Carrinho.jsx
+++ FrontEnd/src/components/pages/client/shop/Carrinho.jsx
@@ -1,10 +1,124 @@
 import { ClienteContext } from "../../../context/ClienteContext";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import apiCliente from "../../../services/apiCliente";
-import { useNavigate } from "react-router-dom";
+import { formatPreco } from "../../../scripts/ProdutoFormValidation";
+import { FaMinusCircle, FaPlusCircle, FaTrash } from "react-icons/fa";
+import styles from "../../../../styles/pages_styles/client_styles/Carrinho.module.css";
 
-function Carrinho(){
+function Carrinho() {
+  const { cliente } = useContext(ClienteContext);
+  const [carrinho, setCarrinho] = useState(null);
 
+  const fetchCarrinho = async () => {
+    try {
+      const response = await apiCliente.get("/cliente/carrinho");
+      setCarrinho(response.data);
+    } catch (error) {
+      console.error("Erro ao buscar carrinho", error);
+    }
+  };
+
+  useEffect(() => {
+    if (cliente) fetchCarrinho();
+  }, [cliente]);
+
+  const handleFinalizarCarrinho = async () => {
+    try {
+      await apiCliente.post("cliente/carrinho/finalizar");
+      alert("Pedido realizado com sucesso!");
+      setCarrinho({ itens: [] });
+    } catch (error) {
+      console.error("Erro ao finalizar pedido", error);
+    }
+  };
+
+  const removerItem = async (produtoId) => {
+    try {
+      await apiCliente.delete(`/cliente/carrinho/item/${produtoId}/remover`);
+      fetchCarrinho();
+    } catch (error) {
+      console.error("Erro ao remover item", error);
+    }
+  };
+
+  const aumentaQuantidade = async (produtoId) => {
+    try {
+      await apiCliente.patch(`/cliente/carrinho/item/${produtoId}/adicionar`);
+      fetchCarrinho();
+    } catch (error) {
+      console.error("Erro ao aumentar quantidade", error);
+    }
+  };
+
+  const diminuirquantidade = async (produtoId) => {
+    try {
+      await apiCliente.patch(`/cliente/carrinho/item/${produtoId}/diminuir`);
+      fetchCarrinho();
+    } catch (error) {
+      console.error("Erro ao diminuir quantidade", error);
+    }
+  };
+
+  if (!cliente)
+    return (
+      <p className={styles.logado}>
+        Você precisa estar logado para acessar o carrinho
+      </p>
+    );
+  if (!carrinho) return <p className={styles.loading}>Carregando...</p>;
+
+  return (
+    <div className={styles.div}>
+      <h1 className={styles.h1}>Seu carrinho</h1>
+      {carrinho.itens.length === 0 ? (
+        <p className={styles.vazio}>Carrinho vazio</p>
+      ) : (
+        <div className={styles.carrinho}>
+          <ul className={styles.ul}>
+            {carrinho.itens.map((item) => (
+              <li key={item._id} className={styles.li}>
+                {item.produtoId.nome} - Quantidade: {item.quantidade} - Preço
+                Unitário: R${formatPreco(item.produtoId.preco)}
+                <div className={styles.buttonGroup}>
+                  <button
+                    onClick={() => diminuirquantidade(item.produtoId._id)}
+                    className={styles.button_d}
+                  >
+                    <FaMinusCircle />
+                  </button>
+                  <button
+                    onClick={() => aumentaQuantidade(item.produtoId._id)}
+                    className={styles.burron_a}
+                  >
+                    <FaPlusCircle />
+                  </button>
+                </div>
+                <div>
+                  <button
+                    onClick={() => removerItem(item.produtoId._id)}
+                    className={styles.button_r}
+                  >
+                    Remover <FaTrash />
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <div className={styles.finalizar}>
+            <p>
+              <strong>Total:</strong> R${carrinho.total?.toFixed(2) || 0}
+            </p>
+            <button
+              onClick={handleFinalizarCarrinho}
+              className={styles.button_finzalizar}
+            >
+              Finalizar Pedido
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Carrinho
+export default Carrinho;
--- FrontEnd/src/components/pages/client/shop/Produtos/CardProduto.jsx
+++ FrontEnd/src/components/pages/client/shop/Produtos/CardProduto.jsx
@@ -1,7 +1,31 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { ClienteContext } from "../../../../context/ClienteContext";
+import apiCliente from "../../../../services/apiCliente";
 import styles from "../../../../../styles/pages_styles/client_styles/cards_styles/CardProduto.module.css"
 function CardProduto({ produto }) {
   const [detalhes, setDetalhes] = useState(false);
+  const[quantidade, setQuantidade] = useState(1)
+  const {cliente} = useContext(ClienteContext)
+
+
+  const handleAdicionarCarrinho = async () => {
+    if(!cliente){
+      alert("Você precisa estar logado como cliente para adicionar ao carrinho")
+      return;
+    }
+
+    try {
+      await apiCliente.post("/cliente/carrinho", {
+        produtoId: produto._id,
+        quantidade: parseInt(quantidade),
+      })
+      alert("Produto adicionado ao carrinho!")
+    } catch (error) {
+      console.error("Error ao adicionar produto ao carrinho", error)
+    }
+
+  }
+
 
   return (
     <div className={styles.cardProduto}>
@@ -29,10 +53,11 @@ function CardProduto({ produto }) {
           </p>
           <label>
             Quantidade:
-            <input type="number" min={1} defaultValue={1} />
+            <input type="number" min={1} defaultValue={1} onChange={(e)=> setQuantidade(e.target.value)}/>
+            
           </label>
-           <button className={styles.btnAdicionarCarrinho}>Adicionar ao Carrinho</button>
-            <button onClick={() => setDetalhes(false)} className={styles.btnDetalhes} style={{ marginTop: '10px', backgroundColor: '#dc3545' }}>Fechar Detalhes</button>
+           <button onClick={handleAdicionarCarrinho} className={styles.btnAdicionarCarrinho}>Adicionar ao Carrinho</button>
+            <button onClick={() => setDetalhes(false)} className={styles.btnDetalhes}>Fechar Detalhes</button>
         </div>
       )}
       </div>
@@ -41,3 +66,5 @@ function CardProduto({ produto }) {
 }
 
 export default CardProduto;
+
+/* style={{ marginTop: '10px', backgroundColor: '#dc3545' }} */
